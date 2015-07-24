a6b107b80ba91152275be1a3e34e69e1c4490b4a
aadesh <aadeshp95@gmail.com>
2015-07-24 19:05:25 -0400

    upload to pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'aws-ec2-ssh',
+    packages = ['aws-ec2-ssh'],
+    version = '0.1',
+    description = 'View all active instances on your AWS EC2 account and easily SSH into any of them',
+    author = 'Aadesh Patel',
+    author_email = 'aadeshp95@gmail.com',
+    url = 'https://github.com/aadeshp/aws-ec2-ssh',
+    download_url = 'https://github.com/aadeshp/aws-ec2-ssh/tarball/0.1',
+    keywords = ['aws', 'ec2', 'ssh'],
+    classifiers = [],
+)
